14a13946e11f1d556fee67178a7a76046bc05b6c
Jacob Shaw <87104264+jacobshaw42@users.noreply.github.com>
2022-10-06 14:50:34 -0500

    Include null modified_on

--- utilization/cadre/users_past_login.sql
+++ utilization/cadre/users_past_login.sql
@@ -1,9 +1,8 @@
 -- Get users and last logged on
 SELECT l.name,
 	u.email,
-	u.modified_on
+	COALESCE(u.modified_on,u.created_on) as last_modified
 FROM user_login l
 JOIN users u
 ON l.id=u.login_id
-WHERE u.modified_on IS NOT NULL
 ORDER BY u.modified_on DESC;
